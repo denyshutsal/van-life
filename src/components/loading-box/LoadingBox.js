2404a1b9ea4f206d2f18403d359ec29bb772868d
Denys Hutsal <denyshutsal@gmail.com>
2023-03-11 17:21:12 +0100

    added LoadingBox component

---
+++ src/components/loading-box/LoadingBox.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./loadingBox.scss";
+
+export default function LoadingBox() {
+  return (
+    <div className="loading-box">
+      <p className="loading-box__text">Loading...</p>
+    </div>
+  );
+}
