1fc1b9f93f375a71951f835fb6690f1ff53c5416
Denys Hutsal <denyshutsal@gmail.com>
2023-03-11 17:16:46 +0100

    added routes and component imports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Vans from "./pages/vans/Vans";
+import VanDetail from "./pages/van-detail/VanDetail";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import "./server";
+import "./normalize.css";
+import "./index.scss";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
