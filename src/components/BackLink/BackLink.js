94d65feaaff9de6f53a9f6853f6aabbb1a168744
Denys Hutsal <denyshutsal@gmail.com>
2023-05-26 21:16:56 +0200

    add a second prop "path"

--- src/components/BackLink/BackLink.js
+++ src/components/BackLink/BackLink.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./BackLink.scss";
 
-export default function BackLink({ text }) {
+export default function BackLink({ text, path = ".." }) {
   return (
-    <Link to=".." relative="path" className="back-link">
+    <Link to={path} relative="path" className="back-link">
       {text}
     </Link>
   );
